8da09c8284796022e6926daed7059cc60137b39f
sorenwilkening <soerenwilkening@gmx.de>
2024-02-21 10:46:38 +0100

    read benchmark instances with benchmark parameters from file executable for instance generator cleanup combowrp.c

--- main.c
+++ main.c
@@ -3,10 +3,21 @@
 #include "stategen.h"
 #include "qaoa_qtg.h"
 
-int main(int argc, const char** argv) {
-    printf("Hello Work.\n");
+int main(int argc, const char **argv) {
 
     knapsack_t *k;
+
+    char instance[1023];
+    char opt_type[32];
+    int p, samples, bias;
+    char line[1023];
+    FILE *file = fopen("../benchmark_instances.txt", "r");
+    while (fgets(line, sizeof(line), file)) { // all the instances will be considered
+        if (line[0] != '#') { // lines startin with '#' are ignored
+            sscanf(line, "%s %d %s %d %d\n", instance, &p, opt_type, &bias, &samples);
+        }
+    }
+    fclose(file);
 
     return 0;
 }
